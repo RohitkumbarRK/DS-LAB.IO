34bc7b52a9a8ed75d1cb0a99aa0a28e54924484b
Rohit Kumabar <143377695+RohitkumbarRK@users.noreply.github.com>
2024-01-12 23:19:50 +0530

    Update binarysearch.cpp implement iterative Binary Search
    Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O.

--- SIKLL LAB/binary search.cpp
+++ SIKLL LAB/binary search.cpp
@@ -7,7 +7,7 @@ int binarySearch(int arr[], int l, int r, int x)
 	while (l <= r) {
 		int m = l + (r - l) / 2;
 
-		// Check if x is present at mid
+		// Check if x is present at mid or not
 		if (arr[m] == x)
 			return m;
 
