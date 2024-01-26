45d451b16240b49217dccc2965a50cb7fffd8f7f
RohitkumbarRK <kumbarrohit3@gmail.com>
2024-01-27 01:43:10 +0530

    no changes

--- DS/suffix.c
+++ DS/suffix.c
@@ -14,7 +14,7 @@ float operate(float,float,char);
 int main()
 {
 int i=0;
-printf("Enter Expression:");
+printf("Enter Expression for postfix convertion:");
 scanf("%s",postfix);
 float ans,op1,op2;
 while(postfix[i]!='\0')
