50399ffb58c937a378356d70110613a8c6f64886
RohitkumbarRK <kumbarrohit3@gmail.com>
2024-02-12 02:27:26 +0530

    sdgsg

--- OS/program 1.cpp
+++ OS/program 1.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<conio.h>
 main()
 {
 int bt[20], wt[20], tat[20], i, n; 
@@ -20,7 +21,7 @@ tat[i] = tat[i-1] +bt[i];
 wtavg = wtavg + wt[i]; 
 tatavg = tatavg + tat[i];
 }
-printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
+printf("\t PROCESS \t\tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]); 
 printf("\nAverage Waiting Time -- %f", wtavg/n);
